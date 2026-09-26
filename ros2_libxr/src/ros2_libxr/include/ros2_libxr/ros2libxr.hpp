bbb337c28aca6317e9ac3e45118f12f3708b8f26
jxy <wuaiwuxin_2019@qq.com>
2026-09-26 15:56:14 +0800

    补交修改后的ros2libxr.hpp

--- ros2_libxr/src/ros2_libxr/include/ros2_libxr/ros2libxr.hpp
+++ ros2_libxr/src/ros2_libxr/include/ros2_libxr/ros2libxr.hpp
@@ -33,6 +33,7 @@
 #include "app_framework.hpp"
 #include "linux_uart.hpp"
 #include "message.hpp"
+#include "std_msgs/msg/int32.hpp"
 #include "thread.hpp"
 #include "uart.hpp"
 #include "SharedTopic/SharedTopic.hpp"
@@ -57,6 +58,11 @@ struct move_vec
   float vx=0.0;
   float vy=0.0;
   float wz=0.0;
+};
+
+struct move_mode
+{
+  uint8_t mode=0;
 };
 
 //云台欧拉角数据结构体
@@ -210,7 +216,7 @@ static void XRobotMain(LibXR::HardwareContainer &hw) {
       appmgr,
       "uart_client",
       16,
-      {{"chassis_data"},{"sentry_state"}}
+      {{"chassis_data"},{"chassis_mode"},{"sentry_state"}}
   );
 }
 
@@ -234,6 +240,8 @@ class RMSerialDriver : public rclcpp::Node {
                                    float &roll, float &pitch, float &yaw);
   void get_classic(const geometry_msgs::msg::Twist::SharedPtr twi);
 
+  void classic(const std_msgs::msg::Int32 mode);
+
  private:
   // LibXR 资源
   std::unique_ptr<LibXR::HardwareContainer> peripherals;
@@ -245,6 +253,7 @@ class RMSerialDriver : public rclcpp::Node {
   // LibXR 话题
   LibXR::Topic ahrs_euler_topic_;
   LibXR::Topic move_vec_topic_;
+  LibXR::Topic move_mode_topic_;
   LibXR::Topic yawmotor_angle_topic_;
   LibXR::Topic sentry_ref_topic_;
   LibXR::Topic ahrs_quaternion_topic_;
@@ -252,13 +261,15 @@ class RMSerialDriver : public rclcpp::Node {
 
   // 底盘运动数据
   move_vec move_;
+  move_mode mode_;
 
   //云台相对底盘yaw全局变量
   float yawmotor_angle_data;
 
   // ROS2 发布者/订阅者
   rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_state_pub_;
-  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr move_vec_sub;
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr move_vec_sub_;
+  rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr move_mode_sub_;
   rclcpp::Publisher<referee_interfaces::msg::RobotStatus>::SharedPtr sentry_ref_pub_;
   rclcpp::Publisher<referee_interfaces::msg::GameStatus>::SharedPtr game_status_pub_;
   rclcpp::Publisher<referee_interfaces::msg::RfidStatus>::SharedPtr rfid_status_pub_;
